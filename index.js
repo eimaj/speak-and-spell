e39faf8a935a577b19144a179ef9d12ef2d0a363
Jamie Allen <jamie@llen.ca>
2018-05-20 15:25:33 -0400

    A bit of an incremental re-write.
    - input was removed
    - body listens to keyup
    - a div renders the output
    - language can be toggled
    - some basic styles were added

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const speak = function (text) {
 
 const state = {
   input: null,
+  output: null,
   clear: null,
   submit: null,
-  
+  languages: [],
+
   text: '',
 
   lang: 'fr',
@@ -22,25 +24,40 @@ const setProperty = function (key, value) {
   return state[key];
 };
 
+const setText = function (text) {
+  setProperty('text', text);
+  return renderOutput(state.text);
+};
+
+const renderOutput = function (text) {
+  state.output.innerText = text;
+  return state.output.innerText;
+};
+
 const actions = {
   speakLetter(e) {
+    console.log(e);
     // catch enter keyCode == 13
     // catch esc keyCode == 27
     // catch tab keyCode == 9: block event
     // catch backspace keyCode == 8: block event
 
-    setProperty('text', state.input.value);
+    setText(state.text + e.key);
     return speak(state.text[state.text.length - 1]);
   },
 
+  changeLanguage(e) {
+    const newLang = e.target.dataset['lang']
+    return setProperty('lang', newLang);
+  },
+
   speakInput() {
-    setProperty('text', state.input.value);
+    setProperty('text', state.text);
     return speak(state.text);
   },
 
-  clearInput() {
-    state.input.value = '';
-    return setProperty('text', state.input.value);
+  clearText() {
+    return setText('');
   },
 };
 
@@ -50,23 +67,53 @@ const app = {
     return state.input.addEventListener('keyup', actions.speakLetter);
   },
 
-  bindSubmit() {
+  bindLanguageButtons() {
+    return state.languages.map(function (language) {
+      return language.addEventListener('click', actions.changeLanguage)
+    });
+  },
+
+  bindSubmitButton() {
     return state.submit.addEventListener('click', actions.speakInput);
   },
 
-  bindClear() {
-    return state.clear.addEventListener('click', actions.clearInput);
+  bindClearButton() {
+    return state.clear.addEventListener('click', actions.clearText);
+  },
+
+  initInput() {
+    setProperty('input', document.querySelector('body'));
+    return this.bindInput();
+  },
+
+  initOutput() {
+    return setProperty('output', document.querySelector('div[name="output"]'));
+  },
+
+  initSubmit() {
+    setProperty('submit', document.querySelector('a[name="speak-input"]'));
+    return this.bindSubmitButton();
+  },
+
+  initClear() {
+    setProperty('clear', document.querySelector('a[name="clear-input"]'));
+    return this.bindClearButton();
+  },
+
+  initLanguages() {
+    const nodeList = document.querySelectorAll('a[name="change-language"]');
+    const languageButtons = Array.from(nodeList);
+
+    setProperty('languages', languageButtons);
+    return this.bindLanguageButtons();
   },
 
   init() {
-    setProperty('input', document.querySelector('input[name="type-here"]'));
-    this.bindInput();
-
-    setProperty('submit', document.querySelector('a[name="speak-input"]'));
-    this.bindSubmit();
-
-    setProperty('clear', document.querySelector('a[name="clear-input"]'));
-    this.bindClear();
+    this.initInput();
+    this.initOutput();
+    this.initSubmit();
+    this.initClear();
+    this.initLanguages();
 
     return this;
   },
