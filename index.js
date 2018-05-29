f1cbe67b6e4a3040a8ba043f0afb5c7b3f3904f0
Jamie Allen <jamie@llen.ca>
2018-05-28 22:21:51 -0400

    Adds numbers to the list of letters

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ const letters = [
   'KeyX',
   'KeyY',
   'KeyZ',
+  'Digit0',
+  'Digit1',
+  'Digit2',
+  'Digit3',
+  'Digit4',
+  'Digit5',
+  'Digit6',
+  'Digit7',
+  'Digit8',
+  'Digit9',
   'Space',
 ];
 
